14929ca55cdc10fd2f9ee28d05022a6e2e59b116
Hang305 <hanglee305@gmail.com>
2021-08-10 17:15:34 +0700

    Create TaskController, Add Task and List Current Task

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -6,7 +6,7 @@
             <!-- Bootstrap Boilerplate... -->
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    {{ __('task.new_task') }}
+                    {{ __('new_task') }}
                 </div>
                 <button><a href="{!! route('user.change-language', ['en']) !!}">{{ __('english') }}</a></button>
                 <button><a href="{!! route('user.change-language', ['vi']) !!}">{{ __('vietnam') }}</a></button>
@@ -15,12 +15,12 @@
                     @include('common.errors')
 
                     <!-- New Task Form -->
-                    <form action="" method="POST" class="form-horizontal">
+                    <form action="{{ route('task.store') }}" method="POST" class="form-horizontal">
                         {{ csrf_field() }}
 
                         <!-- Task Name -->
                         <div class="form-group">
-                            <label for="task" class="col-sm-3 control-label">{{ __('task.task') }}</label>
+                            <label for="task" class="col-sm-3 control-label">{{ __('task') }}</label>
                             <div class="col-sm-6">
                                 <input type="text" name="name" id="task-name" class="form-control">
                             </div>
@@ -30,7 +30,7 @@
                         <div class="form-group">
                             <div class="col-sm-offset-3 col-sm-6">
                                 <button type="submit" class="btn btn-default">
-                                    <i class="fa fa-plus"></i> {{ __('task.add_task') }}
+                                    <i class="fa fa-plus"></i> {{ __('add_task') }}
                                 </button>
                             </div>
                         </div>
@@ -39,5 +39,32 @@
             </div>
         </div>
         <!-- TODO: Current Tasks -->
+        @if (count($tasks) > 0)
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                   {{__('current_task')}}
+                </div>
+
+                <div class="panel-body">
+                    <table class="table table-striped task-table">
+                        <thead>
+                            <th>{{ __('task') }}</th>
+                            <th>&nbsp;</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($tasks as $task)
+                                <tr>
+                                    <td class="table">
+                                        <div>{{ $task->name }}</div>
+                                    </td>
+
+                                    <!-- Task Delete Button -->
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        @endif
     </div>
 @endsection
